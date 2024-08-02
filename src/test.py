bbfda5d568350217d3cec04d8b8322a3b8f08e7f
bryanmelendez <bryanmelendez2323@gmail.com>
2024-08-01 21:33:54 -0700

    learning python

---
+++ src/test.py
@@ -0,0 +1,16 @@
+class Page:
+    def __init__(self, title):
+        self.title = title
+
+    def print_name(self):
+        print(page1.title.title())
+
+    def print_word(self):
+        print("hello")
+
+
+# driver code
+page1 = Page("option 1")
+
+page1.print_name()
+page1.print_word()
