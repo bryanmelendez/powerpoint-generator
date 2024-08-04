ca6c255fe97cc3ca8c7e828a1f81ee6a55ba0ac9
bryanmelendez <bryanmelendez2323@gmail.com>
2024-08-04 11:09:20 -0700

    starting to add images

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from pptx import Presentation
+from pptx.util import Inches
 
-class Slide(object):
+
+class Slide():
 
     # make class attributes stuff like slide dimensions, font sizes, etc
 
@@ -11,10 +13,9 @@ class Slide(object):
 class TitleSlide(Slide):
     def __init__(self, title_text, subtitle_text):
         self.subtitle_text = subtitle_text
+        super().__init__(title_text)
 
-        Slide.__init__(self, title_text)
-
-    def CreateSlide(self):
+    def CreateSlide(self, pres):
         title_slide_layout = pres.slide_layouts[0]
         slide = pres.slides.add_slide(title_slide_layout)
         title = slide.shapes.title
@@ -22,13 +23,31 @@ class TitleSlide(Slide):
         title.text = self.title_text
         subtitle.text = self.subtitle_text
 
+
+class ImageSlide(Slide):
+    def __init__(self, title_text, image_path):
+        self.image_path = image_path
+        super().__init__(title_text)
+
+    def CreateSlide(self, pres):
+        image_slide_layout = pres.slide_layouts[6]
+        slide = pres.slides.add_slide(image_slide_layout)
+
+        # Figure out what layout i need to make it have a title
+        # title = slide.shapes.title
+        # title.text = self.title_text
+
+        left = Inches(1)
+        top = Inches(1)
+        pic = slide.shapes.add_picture(self.image_path, left, top)
+       
 # Driver code
 pres = Presentation()
 
-slide = TitleSlide('Example title',
-                   'Example subtitle')
+title_slide = TitleSlide('Example title', 'Example subtitle')
+title_slide.CreateSlide(pres)
 
-
-slide.CreateSlide()
+image_slide = ImageSlide('Image slide title', '{}Example Exports/Option 1 A.jpg'.format(EXAMPLE_IMAGES))
+image_slide.CreateSlide(pres)
 
 pres.save('test.pptx')
