90cb3c83290864b73e0f3bae505ecc8a7b74bc34
bryanmelendez <bryanmelendez2323@gmail.com>
2024-08-07 15:08:26 -0700

    starting to add pictures and working on messages

--- src/gui.py
+++ src/gui.py
@@ -1,25 +1,29 @@
 # GUI for powerpoint generator
 
 import os
-import tkinter
-from tkinter import ttk
-from tkinter import filedialog
-from tkinter import *
+from tkinter import Tk, Message, ttk, filedialog, StringVar
+from PIL import Image, ImageTk
 
 
 class GUI:
     def __init__(self):
         # Private vars
 
-        self.__root = tkinter.Tk(className='Cara Mia Designs')
+        self.__root = Tk(className='Cara Mia Designs')
         # stores the string from entry box
-        self.__name_entry_string = tkinter.StringVar()
+        self.__name_entry_string = StringVar()
         # the entry box itself
         self.__name_entry_box = None
+        # save file name string
+        self.__save_file_string = StringVar()
+        # save file name entry box
+        self.__save_file_entry_box = None
         # window frame
         self.__frm = None
         self.__status_message = None
         self.__directory_message = None
+        self.__image_label = None
+        self.__image_file = 'assets/logo.png'
 
         # Public vars
 
@@ -33,17 +37,23 @@ class GUI:
         self.__frm.pack()
 
     def create_entry_boxes(self):
-        ttk.Label(self.__root, text='Insert Client Name').pack()
-        self.__name_entry_box = ttk.Entry(self.__root,
+        ttk.Label(self.__frm, text='Insert Client Name').pack()
+        self.__name_entry_box = ttk.Entry(self.__frm,
                                           textvariable=self.__name_entry_string).pack()
+
+        ttk.Label(self.__frm, text='Save file as:').pack()
+        self.__save_file_entry_box = ttk.Entry(self.__frm,
+                                               textvariable=self.__save_file_string).pack()
 
     def submit(self):
         # Getting the string from the tk variable
         self.name = self.__name_entry_string.get()
+        self.file_name = self.__save_file_string.get()
 
-        if self.name == '' or self.directory == '':
+        if self.name == '' or self.directory == '' or self.file_name == '':
             # convert this to gui message later
-            self.set_status_message("No name entered and/or directory selected")
+            self.set_status_message("Error: missing an entry")
+            print("Error: missing an entry")
             return
 
         self.__root.destroy()
@@ -54,21 +64,27 @@ class GUI:
 
         if self.directory == None:
             self.set_directory_message("Error: no directory selected")
+            print("Error: no directory selected")
             return
 
         #update the gui so it shows the path
         self.set_directory_message("Directory selected: {}".format(self.directory))
 
     def set_directory_message(self, message):
-        self.__directory_message = Message(self.__root, text=message)
+        self.__directory_message.config(text=message)
 
     def set_status_message(self, message):
-        self.__status_message = Message(self.__root, text=message)
-        self.__status_message.config(bg='lightgreen')
+        self.__status_message.config(text=message)
 
     def create_gui_window(self):
-        self.__root.geometry("750x250")
+        self.__root.geometry("750x450")
         self.create_frame()
+
+        # adding an image
+        new_image = Image.open(self.__image_file)
+        photo = ImageTk.PhotoImage(new_image)
+        # adding image to label
+        self.__image_label = ttk.Label(self.__frm, image=photo).pack()
 
         self.create_entry_boxes()
 
@@ -77,23 +93,27 @@ class GUI:
         # Folder search button
         ttk.Button(self.__frm, text='Folder Search', command=self.folder_search).pack()
 
-        self.set_directory_message('No directory chosen')
-        self.__directory_message.pack()
+        self.__directory_message = ttk.Label(self.__frm, text='No directory chosen').pack()
 
         # Submit button
         ttk.Button(self.__frm, text='Submit', command=self.submit).pack()
 
-        self.set_status_message('Setup')
-        self.__status_message.pack()
+        self.__status_message = ttk.Label(self.__frm, text="Press 'Submit' when ready").pack()
 
+    # TODO Make this better
     def start_gui(self):
         self.__root.mainloop()
 
+    # Getter functions:
+       
     def get_client_name(self):
         return self.name
 
     def get_file_name(self):
         return self.file_name
+
+    def get_directory_path(self):
+        return self.directory
 
 
 # driver code
@@ -101,6 +121,10 @@ gui = GUI()
 gui.create_gui_window()
 
 gui.start_gui()
+
+print(gui.get_client_name())
+print(gui.get_file_name())
+print(gui.get_directory_path())
 
 
 '''
