e3c2b8bbb0981a13db1ba2f2f89a3f35f213a35e
Bryan Melendez <bryanmelendez2323@gmail.com>
2024-08-16 22:50:42 -0700

    disabled terminal output when running executable

--- install-linux.sh
+++ install-linux.sh
@@ -1,1 +1,1 @@
-pyinstaller --onefile --name powerpoint-generator-linux --add-data=assets/*:assets src/main.py --hidden-import='PIL._tkinter_finder'
+pyinstaller --onefile --name powerpoint-generator-linux --add-data=assets/*:assets src/main.py --hidden-import='PIL._tkinter_finder' --noconsole
--- install-win.bat
+++ install-win.bat
@@ -1,2 +1,2 @@
-pyinstaller --onefile --name powerpoint-generator-windows --add-data=assets/*:assets src/main.py --hidden-import='PIL._tkinter_finder'
+pyinstaller --onefile --name powerpoint-generator-windows --add-data=assets/*:assets src/main.py --hidden-import='PIL._tkinter_finder' --noconsole
  
