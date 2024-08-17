b8470eb11348d70b172614e4201a6ef00583b351
Bryan Melendez <bryanmelendez2323@gmail.com>
2024-08-16 22:54:36 -0700

    renamed executables

--- install-linux.sh
+++ install-linux.sh
@@ -1,1 +1,1 @@
-pyinstaller --onefile --name powerpoint-generator-linux --add-data=assets/*:assets src/main.py --hidden-import='PIL._tkinter_finder' --noconsole
+pyinstaller --onefile --name powerpoint-generator-v0-1-1-linux --add-data=assets/*:assets src/main.py --hidden-import='PIL._tkinter_finder' --noconsole
--- install-win.bat
+++ install-win.bat
@@ -1,2 +1,2 @@
-pyinstaller --onefile --name powerpoint-generator-windows --add-data=assets/*:assets src/main.py --hidden-import='PIL._tkinter_finder' --noconsole
+pyinstaller --onefile --name powerpoint-generator-v0-1-1-windows --add-data=assets/*:assets src/main.py --hidden-import='PIL._tkinter_finder' --noconsole
  
